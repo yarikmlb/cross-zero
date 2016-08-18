4212b29024c41bb0d6dacf79e4f4602436cab8da
marsis <logozynski@gmail.com>
2016-08-18 15:07:00 +0300

    remove obsolete tests

--- src/app/cross/cross.component.spec.ts
+++ src/app/cross/cross.component.spec.ts
@@ -3,11 +3,11 @@
 import { By }           from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { addProviders, async, inject } from '@angular/core/testing';
-import { CrossComponent } from './cross.component';
+import { Cross } from './cross.component';
 
 describe('Component: Cross', () => {
   it('should create an instance', () => {
-    let component = new CrossComponent();
+    let component = new Cross();
     expect(component).toBeTruthy();
   });
 });
